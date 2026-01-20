bef9ed013e05c5eda30b4b40d539afff0ae21626
Jacob Johansson <jacobknuts@gmail.com>
2026-01-20 22:04:56 +0100

    Experimenterade lite med olika sätt att rendera js object i react

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -12,18 +12,23 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
+const Footer = () => {
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
     </div>
   )
 }
 
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
 
 
 export default App
