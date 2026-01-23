20bf32c24f25c3444fb6534123b838887b26309d
Jacob Johansson <jacobknuts@gmail.com>
2026-01-23 21:52:02 +0100

    Done with 1.7: unicafe step 2

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+	const total = good + neutral + bad;
 
     return (
         <div>
@@ -18,6 +19,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+			<p>all {good + neutral + bad}</p>
+			<p>average {(good - bad) / total}</p>
+			<p>positive {good/total * 100}%</p>
         </div>
     );
 };
