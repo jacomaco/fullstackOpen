ca9423c5f842526b9cf517e31f0d3594532d7e56
Jacob Johansson <jacobknuts@gmail.com>
2026-01-23 19:59:03 +0100

    bok u

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fullstackOpen
-bokmärke: https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#complex-state
+bokmärke: https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#exercises-1-6-1-14
--- part1/ReactEvents/src/App.jsx
+++ part1/ReactEvents/src/App.jsx
@@ -3,17 +3,26 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 const App = () => {
-	const [ counter, setCounter ] = useState(0)
+    const [value, setValue] = useState(10);
 
-	setTimeout(
-		() => setCounter(counter + 1),
-		1000
-	)
+    const setToValue = (newValue) => () => {
+        console.log("value now", newValue); // print the new value to console
+        setValue(newValue);
+    };
 
-	return (
-		<div>{counter}</div>
-	)
-}
+	const setOtherValue = (newValue) => {
+		console.log("value now", newValue);
+	}	
 
+    return (
+        <div>
+            {value}
+			<button onClick={setOtherValue(10)}>value</button>
+            <button onClick={setToValue(1000)}>thousand</button>
+            <button onClick={setToValue(0)}>reset</button>
+            <button onClick={setToValue(value + 1)}>increment</button>
+        </div>
+    );
+};
 
 export default App;
