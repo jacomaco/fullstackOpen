c001f2ae448150cd3f5b1dc4cb4b02f127c330bd
Jacob Johansson <jacobknuts@gmail.com>
2026-01-29 15:07:40 +0100

    Done with Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Number = ({ name, number }) => {
   return (
@@ -52,14 +53,21 @@ const Persons = ({ persons, newSearch }) => {
 }
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Artho Hellas", number: "040-1234567", id: 1},
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
+
+  useEffect(() => {
+    console.log('start useEffect');
+    axios
+      .get("http://localhost:3001/persons")
+      .then(request => {
+        console.log('promise fullfilled');
+        setPersons(request.data)
+      })
+  }, [])
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
