6cf087a5fbf78ffa4907ad6385e00bd1319450fc
Jacob Johansson <jacobknuts@gmail.com>
2026-01-30 14:46:15 +0100

    Added service layer

--- reactScratchPad/src/App.jsx
+++ reactScratchPad/src/App.jsx
@@ -1,67 +1,73 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Note from "./components/Note";
+import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note");
-	const [showAll,  setshowAll] = useState(true);
+  const [showAll, setshowAll] = useState(true);
 
   useEffect(() => {
-    console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled');
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }, []) 
-  console.log('render', notes.length, 'notes');
-  
+  }, []);
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
-			content: newNote,
-			important: Math.random() < 0.5,
-		}
+      content: newNote,
+      important: Math.random() > 0.5,
+    };
 
-    axios
-      .post('http://localhost:3001/notes', )
-      .then(response => {
-        console.log(response);
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
       })
-
-		setNotes(notes.concat(noteObject))
-		setNewNote('')
   };
 
-	const handleNoteChange = (event) => {
-		console.log(event.target.value);
-		setNewNote(event.target.value)
-	}
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
 
-	const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
+  };
 
   return (
     <div>
       <h1>Notes</h1>
-			<div>
-				<button onClick={() => {setshowAll(!showAll)}}>
-					show {showAll ? 'important' : 'all'}
-				</button>
-
-			</div>
+      <div>
+        <button onClick={() => setshowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
 
       <form onSubmit={addNote}>
-        <input 
-					value={newNote}
-					onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
