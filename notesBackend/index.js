6331256dc4aa58f7562d955633d71a687dcf6b29
Jacob Johansson <jacobknuts@gmail.com>
2026-02-06 18:58:51 +0100

    prepare for ntoes deploy to render

--- notesBackend/index.js
+++ notesBackend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 
 // pre middleware
 const requestLogger = (request, response, next) => {
@@ -9,7 +11,7 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next(); 
 }
-
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -86,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);  
 })
