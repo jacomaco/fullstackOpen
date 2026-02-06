d0ff05f13945298dfe8758c80851ad84cf2c358e
Jacob Johansson <jacobknuts@gmail.com>
2026-02-06 21:16:53 +0100

    Deploy phonebook to render

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
