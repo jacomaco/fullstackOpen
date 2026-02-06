31d0403993a561adf5c3b63c6307290b6b2d145e
Jacob Johansson <jacobknuts@gmail.com>
2026-02-06 21:27:24 +0100

    uibuild

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static('dist'));
-app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
 let persons = [
