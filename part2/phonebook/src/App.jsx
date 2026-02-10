4bce6eb96509aa10424bdea0fb287dbb776f121b
Jacob Johansson <jacobknuts@gmail.com>
2026-02-10 01:05:17 +0100

    Done with exercise 3.22: All exercises of part 3 are complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import personService from './services/persons';
-import Notification from './components/Notification';
+import { useEffect, useState } from 'react'
+import personService from './services/persons'
+import Notification from './components/Notification'
 
 const Number = ({ person, onClick }) => {
   return (
     <li>
       {person.name} {person.number} <button onClick={() => onClick(person)}>delete</button>
     </li>
-  );
-};
+  )
+}
 
 const PersonForm = ({
   onSubmit,
@@ -20,26 +20,26 @@ const PersonForm = ({
   return (
     <form onSubmit={onSubmit}>
       <div>
-        name: <input type="text" value={newName} onChange={handleNameChange} />
+        name: <input type='text' value={newName} onChange={handleNameChange} />
       </div>
       <div>
-        Number:{" "}
-        <input type="text" value={newNumber} onChange={handleNumberchange} />
+        Number:{' '}
+        <input type='text' value={newNumber} onChange={handleNumberchange} />
       </div>
       <div>
-        <button type="submit">add</button>
+        <button type='submit'>add</button>
       </div>
     </form>
-  );
-};
+  )
+}
 
 const Filter = ({ handleSearchChange, newSearch}) => {
   return (
     <div>
-      filter shown with <input type="text" value={newSearch} onChange={handleSearchChange} />
+      filter shown with <input type='text' value={newSearch} onChange={handleSearchChange} />
     </div>
-  );
-};
+  )
+}
 
 const Persons = ({ persons, newSearch, onClick }) => {
   return (
@@ -53,12 +53,12 @@ const Persons = ({ persons, newSearch, onClick }) => {
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [newSearch, setNewSearch] = useState("");
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
   const [message, setMessage] = useState(null)
-  const [messagetype, setMessageType] = useState("success")
+  const [messagetype, setMessageType] = useState('success')
 
   useEffect(() => {
     personService
@@ -69,13 +69,13 @@ const App = () => {
   }, [])
 
   const handleNameChange = (e) => {
-    setNewName(e.target.value);
-  };
+    setNewName(e.target.value)
+  }
   const handleNumberchange = (e) => {
-    setNewNumber(e.target.value);
-  };
+    setNewNumber(e.target.value)
+  }
   const handleSearchChange = (e) => {
-    setNewSearch(e.target.value);
+    setNewSearch(e.target.value)
   }
   const handleDelete = (person) => {
     if (window.confirm(`Delete: ${person.name} ?`)) {
@@ -88,63 +88,63 @@ const App = () => {
   }
 
   const addName = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const currentPerson = persons.find((person) => person.name.trim() === newName.trim());
+    const currentPerson = persons.find((person) => person.name.trim() === newName.trim())
     if (currentPerson !== undefined) {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        const newObject = {...currentPerson, number: newNumber};
+        const newObject = {...currentPerson, number: newNumber}
         personService
           .update(currentPerson.id, newObject)
           .then(returnedPerson => {
             setPersons(persons.map(person => person.id !== currentPerson.id ? person : returnedPerson))
-            setNewName("");
-            setNewNumber("");
-            setMessageType("success")
+            setNewName('')
+            setNewNumber('')
+            setMessageType('success')
             setMessage(`${newName}'s number was updated to ${newNumber}`)
             setTimeout(() => {
               setMessage(null)
-            }, 5000);
-          }).catch(error => {
-            setNewName("")
-            setNewNumber("")
-            setMessageType("error")
+            }, 5000)
+          }).catch(() => {
+            setNewName('')
+            setNewNumber('')
+            setMessageType('error')
             setMessage(`Information of ${currentPerson.name} has already been removed from server`)
             setPersons(persons.filter(person => person.id !== currentPerson.id))
             setTimeout(() => {
               setMessage(null)
-            }, 5000);
+            }, 5000)
           })
       }
     } else {
       const newNameObject = {
         name: newName,
         number: newNumber,
-      };
+      }
       personService
         .create(newNameObject)
         .then( newPerson => {
-          setPersons(persons.concat(newPerson));
-          setNewName("");
-          setNewNumber("");
-          setMessageType("success")
+          setPersons(persons.concat(newPerson))
+          setNewName('')
+          setNewNumber('')
+          setMessageType('success')
           setMessage(`${newName} was saved!`)
           setTimeout(() => {
             setMessage(null)
-          }, 5000);
+          }, 5000)
         })
         .catch(error => {
-          setNewName("");
-          setNewNumber("");
-          setMessageType("error");
+          setNewName('')
+          setNewNumber('')
+          setMessageType('error')
           setMessage(error.response.data.error)
-          console.log(error.response.data.error);
+          console.log(error.response.data.error)
           setTimeout(() => {
             setMessage(null)
-          }, 5000);
+          }, 5000)
         })
     }
-  };
+  }
 
   return (
     <div>
@@ -162,7 +162,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons persons={persons} newSearch={newSearch} onClick={handleDelete}/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
