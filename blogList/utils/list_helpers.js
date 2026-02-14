ed119c0d8a0974a6386b1a6dd6b19314c5935f7b
Jacob Johansson <jacobknuts@gmail.com>
2026-02-14 06:05:31 +0100

    Done with 4.4

--- blogList/utils/list_helpers.js
+++ blogList/utils/list_helpers.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0,
+  )
+}
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
