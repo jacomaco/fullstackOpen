c1a04c1397f7bc79a1091e6d1bced5176b87ea36
Jacob Johansson <jacobknuts@gmail.com>
2026-04-15 14:34:02 +0200

    Done wtih 5.1

--- bloglist-frontend/src/services/login.js
+++ bloglist-frontend/src/services/login.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const baseUrl = '/api/login'
 
-const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials)
+const login = async credetials => {
+    const response = await axios.post(baseUrl, credetials)
     return response.data
 }
 
