e0570e97ecb12746b4fa56a2aff6a87ed39f7be2
Jacob Johansson <jacobknuts@gmail.com>
2026-04-18 14:29:43 +0200

    Done with 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const simpleView = () => (
+    <>
+      {blog.title} {blog.author} <button onClick={() => toggleVisibility()}>view</button>
+    </>
+  )
+
+  const detailedView = () => (
+    <>
+      <div>
+        {blog.title} {blog.author} <button onClick={() => toggleVisibility()}>hide</button>
+      </div>
+      <div>
+        <a href={blog.url}>{blog.url}</a>
+      </div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user.username}</div>
+    </>
+  )
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div className="blogStyle">
+      {showDetails ? detailedView() : simpleView()}
+    </div>
+  )
+}
 
 export default Blog
