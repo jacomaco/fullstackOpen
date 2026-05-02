c6e8f25b00d7fc40a8d6dd8e0e5da3c525638f24
Jacob Johansson <jacobknuts@gmail.com>
2026-05-02 21:56:14 +0200

    Done with 5.15

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,18 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect } from 'vitest'
 
-describe('5.13', () => {
+describe('<Blog />', () => {
 
-  it('renders the component displaying the blogs title and author, but does not render its URL or number of likes by default.', () => {
-    const blog = {
+  let blog
+
+  beforeEach(() => {
+    blog = {
       title: 'Component testing is done with react-testing-library',
       author: 'Test Author',
       url: 'http://testurl.com',
       likes: 5,
       user: { username: 'testuser', id: '123' }
     }
+  })
+
+  it('renders the component displaying the blogs title and author, but does not render its URL or number of likes by default. (5.13)', () => {
 
     render(<Blog blog={blog} />)
 
@@ -22,27 +26,35 @@ describe('5.13', () => {
     expect(screen.queryByText('http://testurl.com')).toBeNull()
     expect(screen.queryByText(/likes:/i)).toBeNull()
   })
-})
 
-describe('5.14', () => {
-  it('checks that the blogs URL and number of likes are shown when the button controlling the shown details has been clicked.', async () => {
-    const blog = {
-      title: 'Component testing is done with react-testing-library',
-      author: 'Test Author',
-      url: 'http://testurl.com',
-      likes: 5,
-      user: { username: 'testuser', id: '123' }
-    }
+  it('checks that the blogs URL and number of likes are shown when the button controlling the shown details has been clicked. (5.14)', async () => {
 
     render(<Blog blog={blog} />)
 
     const user = userEvent.setup()
-    
+
     const button = screen.getByRole('button', { name: /view/i })
     await user.click(button)
 
     expect(screen.getByText('http://testurl.com')).toBeVisible()
     expect(screen.getByText(/likes:/i)).toBeVisible()
     expect(screen.getByRole('button', { name: /like/i })).toBeVisible()
+  })
+
+  it('ensures that if the like button is clicked twice, the event handler the component received as props is called twice (5.15)', async () => {
+    const likeFunction = vi.fn()
+
+    const user = userEvent.setup()
+
+    render(<Blog blog={blog} incrementLikes={likeFunction} />)
+
+    await user.click(screen.getByRole('button', { name: /view/i }))
+
+    const likeButton = screen.getByRole('button', { name: /like/i })
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(likeFunction).toHaveBeenCalledTimes(2)
   })
 })
---
+++ bloglist-frontend/src/components/createNewBlogs.test.jsx
@@ -0,0 +1,5 @@
+import CreateNewBlog from './CreateNewBlog' 
+
+it('checks, that the form calls the event handler it received as props with the right details when a new blog is created (5.16)', () => {
+
+})
