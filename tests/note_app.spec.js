3b8ba9d7c240ec3e586d58e89bd663eaf4c363fa
Jacob Johansson <jacobknuts@gmail.com>
2026-05-12 18:08:19 +0200

    Starting part 5-D

---
+++ tests/note_app.spec.js
@@ -0,0 +1,23 @@
+const { test, expect, describe } = require('@playwright/test')
+
+describe('Note app', () => {
+    test('front page can be opened', async ({ page }) => {
+        await page.goto('http://localhost:5173/')
+
+        const locator = await page.getByText('Notes')
+        await expect(locator).toBeVisible()
+        await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+    })
+
+    test('user can log in', async ({ page }) => {
+        await page.goto('http://localhost:5173')
+
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.getByLabel('username').fill('mluukkai')
+        await page.getByLabel('password').fill('salainen')
+
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+})
