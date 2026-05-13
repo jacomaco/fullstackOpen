29d26afc498a237c11a021be4925062926e15a6a
Jacob Johansson <jacobknuts@gmail.com>
2026-05-13 16:05:58 +0200

    Refactor app.js to correct router imports and enhance MongoDB connection error handling; add testing router for reset functionality in testing.js; update note_app.spec.js to improve test structure and visibility checks.

--- notesBackend/app.js
+++ notesBackend/app.js
@@ -3,20 +3,20 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/notes')
-const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI, { family: 4 })
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
-  .catch((error) => {
+  .catch(error => {
     logger.error('error connection to MongoDB:', error.message)
   })
 
@@ -24,9 +24,14 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,23 +1,37 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
-    test('front page can be opened', async ({ page }) => {
-        await page.goto('http://localhost:5173/')
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/')
+  })
 
-        const locator = await page.getByText('Notes')
-        await expect(locator).toBeVisible()
-        await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('mluukkai')
+    await page.getByLabel('password').fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
     })
 
-    test('user can log in', async ({ page }) => {
-        await page.goto('http://localhost:5173')
-
-        await page.getByRole('button', { name: 'login' }).click()
-        await page.getByLabel('username').fill('mluukkai')
-        await page.getByLabel('password').fill('salainen')
-
-        await page.getByRole('button', { name: 'login' }).click()
-
-        await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
     })
+  })
 })
