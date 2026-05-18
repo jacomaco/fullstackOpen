5d087d710ad78e47ad33e279ecb7b68ba946caac
Jacob Johansson <jacobknuts@gmail.com>
2026-05-18 18:23:08 +0200

    feat: add react-router-dom and implement client-side routing for notes application

--- notesFrontend/src/components/NoteForm.jsx
+++ notesFrontend/src/components/NoteForm.jsx
@@ -1,31 +1,31 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NoteForm = ({ createNote }) => {
   const [newNote, setNewNote] = useState('')
+  const navigate = useNavigate()
 
-  const addNote = (event) => {
+  const addNote = event => {
     event.preventDefault()
     createNote({
       content: newNote,
       important: true
     })
 
+    navigate('/notes')
     setNewNote('')
   }
+
   return (
     <div>
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <label>
-          content
-          <input
-            value={newNote}
-            onChange={event => setNewNote(event.target.value)}
-            placeholder='write note content here'
-            id='note-input'
-          />
-        </label>
+        <input
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)}
+          placeholder="write note content here"
+        />
         <button type="submit">save</button>
       </form>
     </div>
