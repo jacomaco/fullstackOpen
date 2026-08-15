2f35509cea9def5d194c2fc21acb0df9db530689
Jacob Johansson <jacobknuts@gmail.com>
2026-08-15 09:39:22 +0200

    created zustand followAlong project

---
+++ counter-zustand-test/src/App.jsx
@@ -0,0 +1,12 @@
+import Controls from './components/Controls';
+import Display from './components/Display';
+
+const App = () => {
+	return (
+		<div>
+			<Display />
+			<Controls />
+		</div>
+	);
+};
+export default App
