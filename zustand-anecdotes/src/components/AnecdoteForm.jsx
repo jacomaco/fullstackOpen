4180a3cbc66b95557a433b17fe51f4c9afd1a50a
JacobJohansson <jacobknuts@gmail.com>
2026-08-17 18:09:33 +0200

    Done with 6.2

---
+++ zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,14 @@
+const AnecdoteForm = () => {
+    return (
+        <div>
+            <h2>create new</h2> <form>
+                <div>
+                    <input type="text" />
+                </div>
+                <button>create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
---
+++ zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,22 @@
+import { useAnecdotes, useAnecdoteActions } from '../store'
+
+const AnecdoteList = () => {
+    const anecdotes = useAnecdotes()
+    const { vote } = useAnecdoteActions()
+
+    return (
+        <section>
+            {anecdotes.map(anecdote => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            ))}
+        </section>
+    )
+}
+
+export default AnecdoteList
